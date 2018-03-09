79fe90deb63e353763b8a5a2d7d114127e285fcc
Wail Anaia <wissam.anaia2@gmail.com>
2018-03-09 13:36:15 +0000

    Add function innerHtml

--- js/script.js
+++ js/script.js
@@ -6,43 +6,38 @@ var input   = document.getElementById('input');
 var btn     = document.getElementById('btn');
 var product = document.getElementById('product');
 
-function products(){
+function exist(){
 
-      for(var i= 0; i< productList.length; i++){
-          if(input.value === productList[i]["company"]){
-             if(productList[i]["available"]){
-               product.innerHTML = '<h1>'+ productList[i]["company"] + '</h1>'+
-                                   '<h2> Price : $'+ productList[i]["price"] + '</h2>'+
-                                   '<h2 id="available"> Product is available </h2>';
-               input.value="";
-            } else {
-               product.innerHTML = '<h1>'+ productList[i]["company"] + '</h1>'+
-                                   '<h2> Price : $'+ productList[i]["price"] + '</h2>'+
-                                   '<h2 id="not-available"> Sorry This product is not available </h2>';
-               input.value="";
-             }
-             return true;
-          }
-        }
-      return false;
+  var productAvailable = '<h2 id="available"> Product is available </h2>';
+  var productNotAvailabale = '<h2 id="not-available"> Sorry This product is not available </h2>';
 
+  function innerHtml(x){
+    product.innerHTML = '<h1>'+ productList[i]["company"] + '</h1>'+
+                        '<h2> Price : $'+ productList[i]["price"] + '</h2>'+ x ;
+    input.value="";
+  };
+  for(var i= 0; i< productList.length; i++){
+     if(input.value === productList[i]["company"]){
+       productList[i]["available"]? innerHtml(productAvailable) : innerHtml(productNotAvailabale);
+       return true;
+     }
+  }
+  return false;
+};
+
+function innerHtml(){
+  product.innerHTML = '<h2 id="not-available"> There is no such product </h2>';
+  input.value="";
 };
 
 btn.addEventListener("click",function(){
 
-   if(products()){
-     products();
-   }else {
-     product.innerHTML = '<h2 id="not-available"> There is no such product </h2>';
-     input.value="";
-   }
+   exist()? exist(): innerHtml();
 
 });
 
 input.addEventListener("keydown", function() {
-
-         if (event.keyCode === 13) {
-
-          btn.click();
-      }
+    if (event.keyCode === 13) {
+       btn.click();
+    }
 });
